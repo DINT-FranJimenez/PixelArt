816d9b8074ccd4bc5ba50128fee2bb6631340154
FRANCISCO JIMÉNEZ LOZANO <frajimloz@alu.edu.gva.es>
2023-10-31 19:27:31 +0100

    Cambios hechos pinta Bien con MouseDown y MouseEnter

--- PixelArt/MainWindow.xaml.cs
+++ PixelArt/MainWindow.xaml.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -21,11 +22,15 @@ namespace PixelArt
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        private string backgroundBorder = "Black" ;
+        private int cuadros = 5;
+
         public MainWindow()
         {
             InitializeComponent();
+            Cuadricula_Pequena(cuadros);
 
-            
         }
 
         public void Cuadricula_Pequena(int numeroCuadros) { 
@@ -37,9 +42,13 @@ namespace PixelArt
                 for (int j = 0; j < numeroCuadros; j++) 
                 {
                     Border bd = new Border();
-                    bd.BorderBrush = Brushes.Gray;
                     bd.BorderThickness = new Thickness(1);
-
+                    bd.BorderBrush = Brushes.Gray;
+                    Grid g = new Grid();
+                    g.Style = (Style)FindResource("BackGroundColor");
+                    g.Background = Brushes.White;
+                    bd.Child = g;
+   
                     pixeles.Children.Add(bd);
                 }
  
@@ -51,30 +60,85 @@ namespace PixelArt
         private void Numero_Cuadriculas(object sender, RoutedEventArgs e)
         {
              
-            int cuadros = Convert.ToInt16((sender as Button).Tag);
+            cuadros = Convert.ToInt16((sender as Button).Tag);
             Cuadricula_Pequena(cuadros);
 
         }
 
-        private Color RadioButton_Cambia_Color_Cheked(object sender, RoutedEventArgs e)
+        private void Click_RadioButtonCambiaColor_Chek(object sender, RoutedEventArgs e)
         {
-            var c = new Color();
+            
 
-            if ((sender as RadioButton).Content.Equals("Personalizado"))
-            {
+             if ((sender as RadioButton).Content.Equals("Personalizado"))
+             {
                 if (textBoxColor.Text == null)
-                    c = (Color)ColorConverter.ConvertFromString(textBoxColor.Text);
-                else 
-                { 
-                    c = (Color)ColorConverter.ConvertFromString(textBoxColor.Text);
+                    backgroundBorder = "White";
+                else
+                {
+                    backgroundBorder = textBoxColor.Text;
                 }
-            }
-            else {
-                string aux = Convert.ToString((sender as Button).Tag);
-                c = (Color)ColorConverter.ConvertFromString(aux);
-            }
+             }
+             else
+             { 
+                backgroundBorder =(string)(sender as RadioButton).Tag;
+             }
 
-            return c;
+            
         }
+
+        private void CambiaColor_MouseEnter(object sender, MouseEventArgs e) {
+
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                SolidColorBrush solidColorBrush = new SolidColorBrush((Color)ColorConverter.ConvertFromString(backgroundBorder));
+
+                (sender as Grid).Background = solidColorBrush;
+            }
+         
+
+        }
+
+
+        private void CambiaColor_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                SolidColorBrush solidColorBrush = new SolidColorBrush((Color)ColorConverter.ConvertFromString(backgroundBorder));
+
+                (sender as Grid).Background = solidColorBrush;
+            }
+        }
+
+        private void RellenaTodoUnColor_Click(object sender, RoutedEventArgs e)
+        {
+
+
+            SolidColorBrush solidColorBrush = new SolidColorBrush((Color)ColorConverter.ConvertFromString(backgroundBorder));
+
+            pixeles.Children.Clear();
+
+            for (int i = 0; i < cuadros; i++)
+            {
+                for (int j = 0; j < cuadros; j++)
+                {
+                    Border bd = new Border();
+                    bd.BorderThickness = new Thickness(1);
+                    bd.BorderBrush = Brushes.Gray;
+                    Grid g = new Grid();
+                    g.Style = (Style)FindResource("BackGroundColor");
+                    g.Background = solidColorBrush;
+                    bd.Child = g;
+
+
+
+                    pixeles.Children.Add(bd);
+
+                    
+                }
+
+            }
+        }
+
+       
     }
 }
